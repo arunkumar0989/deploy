c2449ae4b5a3e76ebd33e30ceb67da7c97162531
arunkumar0989 <85949429+arunkumar0989@users.noreply.github.com>
2022-06-12 18:00:14 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const hbs = require('hbs');
+
 
 const test = path.join(__dirname, './html/test.html');
 const prep = path.join(__dirname, '../preparation_views')
 
+app.set('view engine', 'hbs');
+app.set("views", prep);
 app.use(express.static(prep));
 
 app.get('/', (req, res) => {
     res.sendFile(test);
 })
 
-app.listen(process.env.PORT||3000);
+app.listen(process.env.PORT || 3000);
