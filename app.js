0d5fd69e83f75ae27da468549418bb7f7ad1716b
arunkumar0989 <arunkumar0989@yahoo.com>
2022-06-12 17:00:47 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 2033 || process.env.PORT;
+const test = path.join(__dirname, './html/test.html');
+const prep = path.join(__dirname, '../preparation_views')
+
+app.use(express.static(prep));
+
+app.get('/', (req, res) => {
+    res.sendFile(test);
+})
+
+app.listen(port);
